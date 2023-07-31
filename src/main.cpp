12b699b1a4ac992950623d961186080b20e00cc7
Iskender <Iskender>
2023-07-31 14:19:53 +0600

    Added gitignore and new feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 
+void FunctionalityN1();
+
 int main()
 {
     std::cout << "Kind of project\n";
-
+    FunctionalityN1();
 
     std::cin.get();
     return 0;
+}
+
+void FunctionalityN1()
+{
+    std::cout << "New feature number 1\n";
 }
