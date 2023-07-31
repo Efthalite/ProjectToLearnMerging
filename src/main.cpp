f277e318d96fece02083306ae283b5b37400a381
Iskender <Iskender>
2023-07-31 14:38:00 +0600

    Added drive functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 
 
 void FunctionalityN1();
+void Drive();
 void FunctionThatWeDontNeed();
 
 int main()
 {
     std::cout << "Kind of project\n";
     FunctionalityN1();
+    Drive();
 
     std::cin.get();
     return 0;
@@ -16,6 +18,11 @@ int main()
 void FunctionalityN1()
 {
     std::cout << "New feature number 1\n";
+}
+
+void Drive()
+{
+    std::cout << "it is written \"Drive here\" but actually I will drink and sleep\n"; 
 }
 
 void FunctionThatWeDontNeed()
